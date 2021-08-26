2abdefc5b0077536c87a86f6cdfb469d1a02c748
Claudio Carmeli <claudiocarmeli@claudiosmacbook.mynet>
2021-08-26 15:52:46 +0100

    WIP

--- app/controllers/support/cases_controller.rb
+++ app/controllers/support/cases_controller.rb
@@ -1,24 +1,21 @@
-# TODO: remove :nocov: and start testing
-# :nocov:
 module Support
-    #class CasesController < ApplicationController
-    class CasesController < ActionController::Base
-      def index
-        @cases = Support::Cases::Index.new.call
-      end
+  class CasesController < ApplicationController
+  #class CasesController < ActionController::Base
+    def index
+      @cases = Support::Cases::Index.new.call
+    end
 
-      def show
-        @case = Support::Cases::Show.new(id: sanitised_id).call
-      end
+    def show
+      @case = Support::Cases::Show.new(id: sanitised_id).call
+    end
 
-      def update
-      end
+    def update
+    end
 
-      private
-      
-      def sanitised_id
-        params[:id]
-      end
+    private
+    
+    def sanitised_id
+      params[:id].to_s.delete('^0-9')
     end
   end
-  # :nocov:
+end
--- app/services/support/cases/show.rb
+++ app/services/support/cases/show.rb
@@ -9,8 +9,46 @@ module Support
 
       def call
         # Support::Case.find_by(id: id)
-        mock_case_class = Struct.new(:id, :organisation, :status, :category, :assigned_to, :last_updated)
-        mock_case_class.new(1, 'St. Mary', 'NEW', 'MFD', 'John', 1.hour.ago)
+        mock_case_class = Struct.new(
+          :id,
+          :organisation,
+          :status,
+          :category,
+          :assigned_to,
+          :last_updated,
+          :contact_name,
+          :contact_phone,
+          :contact_email,
+          :request_time,
+          :history_events
+        )
+
+        mock_history_event_class = Struct.new(
+          :id,
+          :name,
+          :created_at,
+          :problem_description,
+          :attached_specification,
+          :type
+        )
+
+        mock_case_class.new(
+          1,
+          'St. Mary',
+          'NEW',
+          'MFD',
+          'John',
+          1.hour.ago,
+          'Mary Poppins',
+          '+44 777888123',
+          'mary@poppins.com',
+          2.hour.ago,
+          [
+            mock_history_event_class.new(1, 'foo', 3.hours.ago, 'a', 'b', 'Ticket submission'),
+            mock_history_event_class.new(2, 'bar', 4.hours.ago, 'a', 'b', 'Support ticket'),
+            mock_history_event_class.new(3, 'baz', 5.hours.ago, 'a', 'b', 'Refund request')
+          ]
+        )
       end
     end
   end
